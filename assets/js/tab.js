70b6a7338480df9ca55dfb4d98bf2f0068179f11
Jesper Brynildsen <jesper_bry@hotmail.com>
2018-04-06 23:23:32 +0200

    Add files
    Under development

---
+++ assets/js/tab.js
@@ -0,0 +1,14 @@
+
+function openTab(evt, tabId) {
+  var i, tabcontent, tablink;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+  }
+  tablink = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablink.length; i++) {
+      tablink[i].className = tablink[i].className.replace(" active", "");
+  }
+  document.getElementById(tabId).style.display = "block";
+  evt.currentTarget.className += " active";
+}
